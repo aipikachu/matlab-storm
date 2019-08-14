408faac3fe411fadfb7b753bdc459653c71047c7
Vincent Zhang <aipikachu@users.noreply.github.com>
2019-08-14 19:46:09 +0800

    Create test_myfuncs.m

---
+++ test_myfuncs.m
@@ -0,0 +1,12 @@
+% Simple MATLAB / Octave Code to test Travis CI builds using
+% the .travis.yml file in the root directory of this repository.
+
+%% Correct operations to test
+	A = rand(5)
+	B = rand(5)
+	I = eye(5)
+	C = A./(B+I)
+	c=sqrt(10);
+
+%% Uncomment to create a bug artificially:
+% 	thisIsABug
